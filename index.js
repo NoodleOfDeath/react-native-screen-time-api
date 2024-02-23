ea1f39b0361fe38454545ff4834d9fdc38da3464
noodleofdeath <thom@noodleofdeath.com>
2024-02-23 16:01:54 -0500

    fix missing functions

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const { ScreenTimeAPI } = NativeModules;
 
 const ScreenTime = ScreenTimeAPI;
 
+export const activitySelectionIsEmpty = (selection) => {
+  return !selection || (selection.applicationTokens.length === 0 && selection.categoryTokens.length === 0 && selection.webDomainTokens.length === 0)
+}
+
 const FamilyActivityPickerView = requireNativeComponent('RNTFamilyActivityPickerView');
 
 export { FamilyActivityPickerView, ScreenTime };
