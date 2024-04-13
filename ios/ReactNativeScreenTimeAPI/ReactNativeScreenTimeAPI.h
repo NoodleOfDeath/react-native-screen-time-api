18d0089a19d052ef8a28d0e86886c8f496c7c2b9
noodleofdeath <thom@noodleofdeath.com>
2024-04-13 10:00:20 -0400

    add headers

---
+++ ios/ReactNativeScreenTimeAPI/ReactNativeScreenTimeAPI.h
@@ -0,0 +1,58 @@
+//
+//  ReactNativeScreenTimeAPI.h
+//  ReactNativeScreenTimeAPI
+//
+//  Created by noodleofdeath on 2/18/24.
+//
+
+#import <Foundation/Foundation.h>
+
+#import <React/RCTBridgeModule.h>
+#import <React/RCTViewManager.h>
+
+@interface RCT_EXTERN_MODULE(ScreenTimeAPI, NSObject)
+RCT_EXTERN_METHOD(requestAuthorization: (NSString *) memberName
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(revokeAuthorization: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(getAuthorizationStatus: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(getStore: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(getActivitySelection: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(setActivitySelection: (NSDictionary *) selection
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(clearActivitySelection)
+RCT_EXTERN_METHOD(denyAppRemoval)
+RCT_EXTERN_METHOD(allowAppRemoval)
+RCT_EXTERN_METHOD(denyAppInstallation)
+RCT_EXTERN_METHOD(allowAppInstallation)
+RCT_EXTERN_METHOD(displayFamilyActivityPicker: (NSDictionary *) options
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+RCT_EXTERN_METHOD(initializeMonitoring: (NSString *) start
+                  end: (NSString *) end
+                  resolver: (RCTPromiseResolveBlock) resolve
+                  rejecter: (RCTPromiseRejectBlock) reject)
+@end
+
+@interface RNTFamilyActivityPickerViewFactory: NSObject
++ (UIView *)view;
+@end
+
+@interface RNTFamilyActivityPickerViewManager: RCTViewManager
+@end
+
+@implementation RNTFamilyActivityPickerViewManager
+
+RCT_EXPORT_MODULE(RNTFamilyActivityPickerView)
+
+- (UIView *)view
+{
+  return [RNTFamilyActivityPickerViewFactory view];
+}
+
+@end
