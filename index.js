506bf92415110ab2c45624e2fddaf4ba09688e8c
noodleofdeath <thom@noodleofdeath.com>
2024-07-22 09:43:42 -0400

    update isEmpty

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const ScreenTime = ScreenTimeAPI;
 
 export const activitySelectionIsEmpty = (selection) => {
   if (!selection) return true;
-  return Object.values(selection).every((v) => !v || Array.isArray(v) && v.length === 0);
+  return Object.values(selection).every((v) => !v || (Array.isArray(v) && v.length === 0));
 }
 
 const FamilyActivityPickerView = requireNativeComponent('RNTFamilyActivityPickerView');
